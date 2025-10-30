5255e39bbcfe0c95e5bd7a4eeea5e3bf0fb22aa7
adarsh <adarshnishadd777@gmail.com>
2025-10-30 19:59:41 +0530

    install react-redux, reduxjs/toolkit and make user slice and appstore for storing the data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,23 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Body from "./pages/Body";
-import Login from "./pages/Login"
+import Login from "./pages/Login";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login/>} />
-            {/* we need to add <Outlet/> in <Body/> file for using the children */}
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              {/* we need to add <Outlet/> in <Body/> file for using the children */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
