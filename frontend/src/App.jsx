57b4f889a29727f3083b764fcc6fa3cc838a4a3e
Adarsh Kumar Nishad <69967272+adars777@users.noreply.github.com>
2025-12-01 21:58:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Provider store={appStore}>
         <BrowserRouter basename="/">
           <Routes>
-            <Route path="/" element={<Login />}>
+            <Route path="/" element={<Body />}>
               <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
