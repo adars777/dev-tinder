46d60adbf9fcd1ba796d726976873501c5b27ef6
adarsh <adarshnishadd777@gmail.com>
2026-01-10 23:38:53 +0530

    refactor: reorganize imports and update routing structure in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
+import { Provider } from "react-redux";
+
+import appStore from "./utils/appStore";
 import Body from "./pages/Body";
 import Login from "./pages/Login";
-import { Provider } from "react-redux";
-import appStore from "./utils/appStore";
+import Register from "./pages/Register";
 import Feed from "./pages/Feed";
 import Profile from "./pages/Profile";
 import Connections from "./pages/Connections";
 import Requests from "./pages/Requests";
-import Register from "./pages/Register";
 import Chat from "./components/Chat";
 
 function App() {
   return (
-    <>
-      <Provider store={appStore}>
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path="/feed" element={<Feed />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/connections" element={<Connections />} />
-            <Route path="/requests" element={<Requests />} />
-            <Route path="/chat/:targetUserId" element={<Chat />} />
-          </Route>
-        </Routes>
-      </Provider>
-    </>
+    <Provider store={appStore}>
+      <Routes>
+        <Route path="/" element={<Body />}>
+          {/* DEFAULT ROUTE */}
+          <Route index element={<Navigate to="/login" replace />} />
+
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="feed" element={<Feed />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="connections" element={<Connections />} />
+          <Route path="requests" element={<Requests />} />
+          <Route path="chat/:targetUserId" element={<Chat />} />
+        </Route>
+      </Routes>
+    </Provider>
   );
 }
 
